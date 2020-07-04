b4d7d6ba9add2ec505d3c754e11adc9b9a58583f
Nikita Krivosheyev <jast.e.new.name@gmail.com>
2020-07-04 07:00:36 +0300

    Implemented 4 stage - Action!

--- Coffee Machine/task/src/machine/Main.kt
+++ Coffee Machine/task/src/machine/Main.kt
@@ -2,26 +2,48 @@ package machine
 
 import java.util.*
 
-fun main() {
-    val scanner = Scanner(System.`in`)
-    print("Write how many ml of water the coffee machine has: > ")
-    var canMake = scanner.nextInt() / 200
-    print("Write how many ml of milk the coffee machine has: > ")
-    val milk = scanner.nextInt() / 50
-    if (milk < canMake) {
-        canMake = milk
+val scanner = Scanner(System.`in`)
+fun printState(water: Int, milk: Int, beans: Int, cups: Int, dollars: Int) {
+    println("The coffee machine has:\n" +
+            "${1200 + water} of water\n" +
+            "${540 + milk} of milk\n" +
+            "${120 + beans} of coffee beans\n" +
+            "${9 + cups} of disposable cups\n" +
+            "${550 + dollars} of money")
+}
+
+fun buy() {
+    print("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino: >")
+    when (scanner.nextInt()) {
+        1 -> printState(-250, 0, -16, -1, 4)
+        2 -> printState(-350, -75, -20, -1, 7)
+        3 -> printState(-200, -100, -12, -1, 6)
     }
-    print("Write how many grams of coffee beans the coffee machine has: > ")
-    val beans = scanner.nextInt() / 15
-    if (beans < canMake) {
-        canMake = beans
-    }
-    print("Write how many cups of coffee you will need: > ")
+}
+
+fun fill() {
+    print("Write how many ml of water do you want to add: >")
+    val water = scanner.nextInt()
+    print("Write how many ml of milk do you want to add: >")
+    val milk = scanner.nextInt()
+    print("Write how many grams of coffee beans do you want to add: >")
+    val beans = scanner.nextInt()
+    print("Write how many disposable cups of coffee do you want to add: >")
     val cups = scanner.nextInt()
-    if (cups > canMake) {
-        println("No, I can make only $canMake cups of coffee")
-    } else {
-        print("Yes, I can make that amount of coffee")
-        println(if (canMake > cups) " (and even ${canMake - cups} more than that)" else "")
+    printState(water, milk, beans, cups, 0)
+}
+
+fun take() {
+    println("I gave you \$550")
+    printState(0, 0, 0, 0, -550)
+}
+
+fun main() {
+    printState(0, 0, 0, 0, 0)
+    print("Write action (buy, fill, take): >")
+    when (scanner.next()) {
+        "buy" -> buy()
+        "fill" -> fill()
+        "take" -> take()
     }
 }
