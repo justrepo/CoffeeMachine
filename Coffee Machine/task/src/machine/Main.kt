40772384b7e18ad184d41a0bb3c0ae1977328291
Nikita Krivosheyev <jast.e.new.name@gmail.com>
2020-07-04 07:01:31 +0300

    Implemented 5 stage - On a coffee loop

--- Coffee Machine/task/src/machine/Main.kt
+++ Coffee Machine/task/src/machine/Main.kt
@@ -2,22 +2,39 @@ package machine
 
 import java.util.*
 
+var currentWater = 400
+var currentMilk = 540
+var currentBeans = 120
+var currentCups = 9
+var currentDollars = 550
 val scanner = Scanner(System.`in`)
-fun printState(water: Int, milk: Int, beans: Int, cups: Int, dollars: Int) {
+fun printState() {
     println("The coffee machine has:\n" +
-            "${1200 + water} of water\n" +
-            "${540 + milk} of milk\n" +
-            "${120 + beans} of coffee beans\n" +
-            "${9 + cups} of disposable cups\n" +
-            "${550 + dollars} of money")
+            "$currentWater of water\n" +
+            "$currentMilk of milk\n" +
+            "$currentBeans of coffee beans\n" +
+            "$currentCups of disposable cups\n" +
+            "\$$currentDollars of money")
+}
+
+fun changeState(water: Int, milk: Int, beans: Int, cups: Int, dollars: Int) {
+    if (currentWater + water < 0) {
+        println("Sorry, not enough water!")
+    } else {
+        currentWater += water
+        currentMilk += milk
+        currentBeans += beans
+        currentCups += cups
+        currentDollars += dollars
+    }
 }
 
 fun buy() {
-    print("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino: >")
-    when (scanner.nextInt()) {
-        1 -> printState(-250, 0, -16, -1, 4)
-        2 -> printState(-350, -75, -20, -1, 7)
-        3 -> printState(-200, -100, -12, -1, 6)
+    print("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu: >")
+    when (scanner.next()) {
+        "1" -> changeState(-250, 0, -16, -1, 4)
+        "2" -> changeState(-350, -75, -20, -1, 7)
+        "3" -> changeState(-200, -100, -12, -1, 6)
     }
 }
 
@@ -30,20 +47,23 @@ fun fill() {
     val beans = scanner.nextInt()
     print("Write how many disposable cups of coffee do you want to add: >")
     val cups = scanner.nextInt()
-    printState(water, milk, beans, cups, 0)
+    changeState(water, milk, beans, cups, 0)
 }
 
 fun take() {
-    println("I gave you \$550")
-    printState(0, 0, 0, 0, -550)
+    println("I gave you \$$currentDollars")
+    changeState(0, 0, 0, 0, -currentDollars)
 }
 
 fun main() {
-    printState(0, 0, 0, 0, 0)
-    print("Write action (buy, fill, take): >")
-    when (scanner.next()) {
-        "buy" -> buy()
-        "fill" -> fill()
-        "take" -> take()
-    }
+    do {
+        print("Write action (buy, fill, take, remaining, exit): >")
+        val action = scanner.next()
+        when (action) {
+            "buy" -> buy()
+            "fill" -> fill()
+            "take" -> take()
+            "remaining" -> printState()
+        }
+    } while (action != "exit")
 }
