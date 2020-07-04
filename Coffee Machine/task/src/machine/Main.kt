0d3289385b97aefab54e1995ead4a606033a84be
Nikita Krivosheyev <jast.e.new.name@gmail.com>
2020-07-04 06:59:36 +0300

    Implemented 3 stage - Enough coffee for everyone

--- Coffee Machine/task/src/machine/Main.kt
+++ Coffee Machine/task/src/machine/Main.kt
@@ -4,10 +4,24 @@ import java.util.*
 
 fun main() {
     val scanner = Scanner(System.`in`)
+    print("Write how many ml of water the coffee machine has: > ")
+    var canMake = scanner.nextInt() / 200
+    print("Write how many ml of milk the coffee machine has: > ")
+    val milk = scanner.nextInt() / 50
+    if (milk < canMake) {
+        canMake = milk
+    }
+    print("Write how many grams of coffee beans the coffee machine has: > ")
+    val beans = scanner.nextInt() / 15
+    if (beans < canMake) {
+        canMake = beans
+    }
     print("Write how many cups of coffee you will need: > ")
-    val n = scanner.nextInt()
-    print("For " + n + " cups of coffee you will need:\n"
-            + n * 200 + " ml of water\n"
-            + n * 50 + " ml of milk\n"
-            + n * 15 + " g of coffee beans")
+    val cups = scanner.nextInt()
+    if (cups > canMake) {
+        println("No, I can make only $canMake cups of coffee")
+    } else {
+        print("Yes, I can make that amount of coffee")
+        println(if (canMake > cups) " (and even ${canMake - cups} more than that)" else "")
+    }
 }
